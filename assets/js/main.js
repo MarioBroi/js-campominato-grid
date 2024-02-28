5c74ef2172d89ebf96ca7e7537a94199b5a529d8
MarioBroi <inoix16@gmail.com>
2024-02-28 13:27:11 +0000

    add container variable and for loop to create 100 slots every slot containe a number from 1 to 100

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,20 @@
-console.log('test');
+//console.log('test');
+
+let container = document.getElementsByClassName('container')
+//console.log(container);
+
+//- Creare il bottone di inizio e collegerlo al main.js con la funzione onclick ✔
+
+//- Creare uno script che genera una griglia da 10 righe 
+//- la griglia deve avere un totale di 10 caselle
+for (let i = 0; i < 100; i++) {
+const slot = document.createElement("div");
+slot.classList.add("square");
+
+//- la griglia deve avere un numero univoco tra 1 e 100
+slot.innerText = i + 1
+console.log(slot);
+
+}
+
+//- Al cliccare della della cella, si aggiungerá la classe con il color azzurro e apparirá in console con il cell number
