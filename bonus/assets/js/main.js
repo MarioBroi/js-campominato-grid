7ebd47d33907067a9964278bac47860ced742504
MarioBroi <inoix16@gmail.com>
2024-02-28 15:03:49 +0000

    edit custom difficulty selection the player can now select the difficulty and play whit different types of grids

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,26 +1,45 @@
 //console.log('test');
 
-
 //- Creare il bottone di inizio e collegerlo al main.js con la funzione onclick ✔
-const buttonElement = document.querySelector('.start-button')
+const buttonElement = document.querySelector('.start-button');
+
 buttonElement.addEventListener('click', function () {
 
     //Definisco l'area di lavoro
-    const container = document.querySelector('.container')
+    const container = document.querySelector('.container');
     //console.log(container);
+
+    //Definisco la difficoltá selezionata
+    const difficulty = parseInt(document.getElementById("difficulty").value);
+    //console.log(difficulty); //valore della select
+
+    //- Moltiplico il numero di celle per se stesso in modo tale da avere il num di celle corretto
+    const gridSize = difficulty * difficulty;
+    //console.log(gridSize);
 
     //Pulisco il container dal bottone e lo preparo per le caselle
     container.innerHTML = "";
 
-    //- Creare uno script che genera una griglia da 10 righe 
-    //- la griglia deve avere un totale di 10 caselle
-    for (let i = 0; i < 100; i++) {
+    //Cambiamo impaginazione in base alla difficoltá
+    if (difficulty === 7) {
+        container.classList.add('easy-container');
+        container.classList.remove('medium-container', 'hard-container');
+    } else if (difficulty === 9) {
+        container.classList.add('medium-container');
+        container.classList.remove('easy-container', 'hard-container');
+    } else if (difficulty === 10) {
+        container.classList.add('hard-container');
+        container.classList.remove('easy-container', 'medium-container');
+    }
+
+    //- Creare la griglia in base alla difficoltá scelta
+    for (let i = 0; i < gridSize; i++) {
         //- Creo un elemento div e aggiungo la classe square
         const slot = document.createElement('div');
         slot.classList.add('square');
 
-        //- la griglia deve avere un numero univoco tra 1 e 100
-        slot.innerText = i + 1
+        //- la griglia deve avere un numero univoco tra 1 e il massimo dettato dalla difficoltá
+        slot.innerText = i + 1;
         //console.log(slot);
 
         //- Al cliccare della della cella, si aggiungerá la classe con il color azzurro e apparirá in console con il cell number
@@ -33,6 +52,5 @@ buttonElement.addEventListener('click', function () {
         //- Aggiungo la casella a container
         container.append(slot);
     }
-
 
 })
