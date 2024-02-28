940a0c8fcae3f76bb58acf58f1a79a65df11224c
MarioBroi <inoix16@gmail.com>
2024-02-28 13:59:22 +0000

    add button to start start the game at the click of the button the loop gonna start and generate the slots to play the game

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
 //console.log('test');
 
-let container = document.getElementsByClassName('container')
-//console.log(container);
 
 //- Creare il bottone di inizio e collegerlo al main.js con la funzione onclick ✔
+const buttonElement = document.querySelector(".start-button")
+buttonElement.addEventListener ('click', function()  {
 
-//- Creare uno script che genera una griglia da 10 righe 
-//- la griglia deve avere un totale di 10 caselle
-for (let i = 0; i < 100; i++) {
-const slot = document.createElement("div");
-slot.classList.add("square");
+    //Definisco l'area di lavoro
+    let container = document.querySelector('.container')
+    //console.log(container);
 
-//- la griglia deve avere un numero univoco tra 1 e 100
-slot.innerText = i + 1
-console.log(slot);
+    //Pulisco il container dal bottone e lo preparo per le caselle
+    container.innerHTML = "";
+    
+    //- Creare uno script che genera una griglia da 10 righe 
+    //- la griglia deve avere un totale di 10 caselle
+    for (let i = 0; i < 100; i++) {
+    const slot = document.createElement("div");
+    slot.classList.add("square");
+    
+    //- la griglia deve avere un numero univoco tra 1 e 100
+    slot.innerText = i + 1
+    console.log(slot);
+    
+    //-Aggiungo la casella a container
+    container.append(slot);
+    }
 
-}
+    
+})
 
 //- Al cliccare della della cella, si aggiungerá la classe con il color azzurro e apparirá in console con il cell number
