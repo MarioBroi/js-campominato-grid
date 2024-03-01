a86b22a8d64f32de2a3f0be05b7f70ac3a9b32ee
MarioBroi <inoix16@gmail.com>
2024-03-01 13:49:35 +0000

    edit main script to refresh the game when game over

--- MagicField/assets/js/main.js
+++ MagicField/assets/js/main.js
@@ -1,20 +1,19 @@
-let mushroomArray = [];
-let punteggio = 0;
+/* IIFE */
 
 /**
- * Inizia il gioco IIFE
+ * Starts the game
  */
 (function () {
-    console.log('hi there');
     // select the magic field
     const magicField = document.getElementById('field');
+    // define max cells numbers
 
     // select the play button from the dom to start the game
     document.querySelector('form').addEventListener('submit', function (e) {
 
         // Prevent default behaviour
         e.preventDefault();
-        // Read the selected level
+        //Read the selected level
         console.log(e.target.level.value);
         const cellsNumber = e.target.level.value;
         // Use the selected level to generate the magic field
@@ -24,108 +23,123 @@ let punteggio = 0;
 
         generateMagicField(magicField, cellsNumber);
 
-        //- (while) lopp over until the empty array has 16 elements (not repeated)
-        while (mushroomArray.length < 16) {
-            // inside the loop
-            //console.log(cellsNumber);
-
-            const randomNumber = getRandomInteger(1, cellsNumber);
-            //console.log(randomNumber);
-
-            // check if the number is already inside the array?
-            if (!mushroomArray.includes(randomNumber)) {
-                // otherwise push it
-                mushroomArray.push(randomNumber);
-            }
-            console.log(mushroomArray);
-        };
-
-    });
+    })
 
 })();
 
-/**
- * Crea un numero random tra la difficoltá scelta
- * @param {number} min 
- * @param {number} max 
- * @returns 
- */
-function getRandomInteger(min, max) {
+function generateMushrooms(level) {
+
+    const mushrooms = [];
+
+    while (mushrooms.length < 16) {
+        const randomNumber = getRndInteger(1, level);
+        if (!mushrooms.includes(randomNumber)) {
+            mushrooms.push(randomNumber);
+        }
+
+    }
+
+    return mushrooms;
+}
+
+
+function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
-};
+}
 
 /**
- * Genera il campo prima pulendo il campo stesso, 
- * poi richiama la funzione generateMagicCell loopata in base alla difficoltá
+ * 
  * @param {*} fieldDomElement 
  * @param {*} level 
  */
 function generateMagicField(fieldDomElement, level) {
     // Empty the magic filed before adding new cells
     fieldDomElement.innerHTML = '';
+
+    // generate mushrooms
+    const mushrooms = generateMushrooms(level)
+    console.log(mushrooms.sort((a, b) => a - b));
+
     // generate the magic field 🍄
     for (let i = 1; i <= level; i++) {
 
         // generate the single cell
         const nodeCellElement = generateMagicCell(i, level);
 
-        //console.log(nodeCellElement);
+        console.log(nodeCellElement);
+
+        if (mushrooms.includes(i)) {
+            nodeCellElement.querySelector('span').innerText = '🍄'
+        }
 
         // add to the cell an event listener
-        attachEventToMagicCell(nodeCellElement);
+        attachEventToMagicCell(nodeCellElement)
 
-        fieldDomElement.insertAdjacentElement('beforeend', nodeCellElement);
+        fieldDomElement.insertAdjacentElement('beforeend', nodeCellElement)
 
-    };
+    }
 
-};
+}
 
 /**
- * Al click sul nodo a questo viene aggiunta la classe bg-active
+ * 
  * @param {*} node 
  */
 function attachEventToMagicCell(node) {
-    node.addEventListener('click', handleClick);
+    node.addEventListener('click', handleClick)
 }
 
 function handleClick(e) {
     // add a active class to the clicked element
-    //console.log(this, e); // this é il nodo della dom in questo contesto - e é l'evento triggerato
-    console.log(parseInt(this.innerText));
+    console.log(this, e); // this é il nodo della dom in questo contesto - e é l'evento triggerato
+    this.classList.toggle('bg-active');
+    // print into the console the cell number
+    if (this.querySelector('span').innerText == '🍄') {
+        this.classList.remove('bg-active');
+        this.classList.add('red');
+        this.querySelector('span').style.display = 'inline';
+        // end game
+        gameOver();
+    }
 
-    if (mushroomArray.includes(parseInt(this.innerText)) == true) {
-        console.log('perso');
+    // Check if the user has clickd on the last available cell based on the field generated
+    const totalCells = document.querySelectorAll('.cell').length;
+    const clickedCells = document.getElementsByClassName('bg-active').length;
 
-        this.classList.toggle('red')
-        this.innerText = '⚰️';
-        //print the class whit red color
-        /* 
-                (!alert('You Lose, want to retry?')) {
-                    wondow.location.reload();
-                } */
-    } else {
-        console.log('vinto');
-        this.classList.toggle('green')
-        punteggio = punteggio + 1
-        console.log(punteggio);
-        //print the class whit green color
+    if (totalCells - clickedCells === 16) {
+        gameOver();
     }
 
 }
 
 function gameOver() {
-    const score = document.getElementsByClassName('red').length;
+    console.log('game over');
+    // find the score
+    const score = document.getElementsByClassName('bg-active').length;
 
-    const cells = document.querySelectorAll('cells');
+    console.log('your score: ', score);
+    // prevent the click to all cell
+    const cells = document.querySelectorAll('.cell');
 
     for (let i = 0; i < cells.length; i++) {
         const node = cells[i];
-        node.removeEventListener('click', handleClick)
+        node.removeEventListener('click', handleClick);
+        // Show all mushrooms when the game ends
+        const spanEl = node.querySelector('span');
+        if (spanEl.innerText === '🍄') {
+            node.classList.add('red');
+            spanEl.style.display = 'inline';
+        }
+
     }
+    const gameOverElement = document.getElementById('game-over')
+    gameOverElement.style.display = 'block';
+    gameOverElement.querySelector('span').innerText = score;
+
 }
 
 /**
- * Generate a cell whit a div, class cell and number inside
+ * 
  * @param {*} numb 
  * @param {*} size 
  * @returns 
@@ -133,7 +147,12 @@ function gameOver() {
 function generateMagicCell(numb, size) {
     const nodeCellElement = document.createElement('div');
     nodeCellElement.classList.add('cell');
-    nodeCellElement.innerText = numb;
+
+    const spanEl = document.createElement('span');
+    spanEl.innerText = numb;
+
+    nodeCellElement.appendChild(spanEl);
+    nodeCellElement.style.width = `calc(100% / ${Math.sqrt(size)})`;
 
     return nodeCellElement;
 }
